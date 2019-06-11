183339c5b3b60beb4508b33cdf0d8b7a6fc66c37
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-06-10 20:13:05 -0400

    Updated Styles and Started on JS
     - Added js with jQuery for button click detection
     - Updated styles to make popup visually appealing (added gradient and centered text)

---
+++ src/js/popup.js
@@ -0,0 +1,17 @@
+
+window.onload = function () {
+  updateSettings();
+  $('#toggle-enable').click(toggleSettings);
+}
+
+setInterval(function () {
+    updateSettings();
+}, 500);
+
+function updateSettings () {
+
+}
+
+function toggleSettings () {
+
+}
