f21c194ff22673969b360f665217ce9de336f9c3
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-06-21 15:40:49 -0400

    Update content.js
     - Updated from using nodeName to tagName instead in highlight appending and unappending

--- src/js/content.js
+++ src/js/content.js
@@ -19,24 +19,49 @@ chrome.runtime.onMessage.addListener(
 document.addEventListener('mousemove', function (e) {
 
   if (enabled) {
+
+    // Go through highlighting/appending procedure throughout the DOM
     var srcElement = e.srcElement;
 
     // Lets check if our underlying element is a DIV.
-    if (srcElement.nodeName == 'DIV') {
+    if (srcElement.tagName == 'DIV') {
 
-      // For NPE checking, we check safely. We need to remove the class name
-      // Since we will be styling the new one after.
-      if (prevDOM != null) {
-        prevDOM.classList.remove(HIGHLIGHT_CLASS);
-      }
+        // For NPE checking, we check safely. We need to remove the class name
+        // Since we will be styling the new one after.
+        if (prevDOM != null) {
+          prevDOM.classList.remove(HIGHLIGHT_CLASS);
+        }
 
-      // Add a visited class name to the element. So we can style it.
-      srcElement.classList.add(HIGHLIGHT_CLASS);
+        // Add a visited class name to the element. So we can style it.
+        srcElement.classList.add(HIGHLIGHT_CLASS);
 
-      // The current element is now the previous. So we can remove the class
-      // during the next iteration.
-      prevDOM = srcElement;
+        // The current element is now the previous. So we can remove the class
+        // during the next iteration.
+        prevDOM = srcElement;
     }
+
+  } else if (!enabled) {
+
+    // Go through unhighlighting/unappending procedure throughout the DOM
+    var srcElement = e.srcElement;
+
+    // Lets check if our underlying element is a DIV.
+    if (srcElement.tagName == 'DIV') {
+
+        // For NPE checking, we check safely. We need to remove the class name
+        // Since we will be styling the new one after.
+        if (prevDOM != null) {
+          prevDOM.classList.remove(HIGHLIGHT_CLASS);
+        }
+
+        // Add a visited class name to the element. So we can style it.
+        srcElement.classList.remove(HIGHLIGHT_CLASS);
+
+        // The current element is now the previous. So we can remove the class
+        // during the next iteration.
+        prevDOM = srcElement;
+    }
+
   }
 
 }, false);
