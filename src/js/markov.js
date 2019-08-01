b8240eda0c64e6a58f9498508b530ece952458d1
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-08-01 00:50:38 -0400

    Update markov.js
     - Created a new class called MarkovModel which acts as a markov text chain summarizer with cap length
     - Added prototypes to MarkovModel object

--- src/js/markov.js
+++ src/js/markov.js
@@ -1,4 +1,28 @@
 
 var markovjs = (function() {
-  
+
+  var MarkovModel = function (inputText) {
+  }
+
+  MarkovModel.prototype = {
+
+    prep: function (inputText, capLength) {
+
+    },
+
+    summarize: function (inputText, capLength) {
+
+    }
+
+  }
+
+  function getRand (seed) {
+    return Math.random()*seed;
+  }
+
+  // Exported elements: markov model with highlighted text data prepped for summarization
+  let exports = {};
+  exports.MarkovModel = MarkovModel;
+  return exports;
+
 })();
