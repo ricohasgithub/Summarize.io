65594d3c7621d3db974812afe0462292ac48e4cf
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-08-10 11:34:28 -0400

    Completed prep Function
     - Completed the prep function in markov.js
     - Added follower array filling code

--- src/js/markov.js
+++ src/js/markov.js
@@ -8,24 +8,63 @@ var markovjs = (function() {
 
     prep: function (inputText, capLength) {
 
+      inputText = this.parseWord(inputText);
+
       this.inputText = inputText;
       this.capLength = capLength;
 
       let followers = new Array(inputText.length);
 
       for (i=0; i<inputText.length; i++) {
-        // Create a new array of length 4 (4 most significant words)
+        // Create a new array of length 4 (key word + 3 following words)
         folowers[i] = new Array(4);
       }
 
       // Fill the follower array
       for (i=0; i<inputText.length; i++) {
 
-        let currWord = this.parseWord(inputText[i]);
+        let currWord = inputText[i];
 
+        // Set the key
+        let wordArr = followers[i];
+        wordArr[0] = currWord;
+
+        // Check for duplicates; copy over if exists
+        for (j=0; j<i; j++) {
+          if (followers[j][0] === currWord) {
+            followers[i] = followers[j];
+          }
+        }
+
+        // This trakcs how many followers have been added and the index of teh current array value
+        let count = 1;
+
+        // Add following words for each given word
+       for (j=0; j<inputText.length-1; j++) {
+
+         if (inputText[j] === currWord  && count < 4) {
+
+           let found = false;
+           let next = inputText[j+1];
+
+           for (k=0; k<wordArr.length; k++) {
+             if (wordArr[k] === currWord) {
+               found = true;
+               break;
+             }
+           }
+
+           if (!found) {
+             wordArr[count] = next;
+           }
+
+         }
+
+       }
 
       }
 
+      this.followers = followers;
 
     },
 
@@ -37,6 +76,7 @@ var markovjs = (function() {
 
     parseWord: function (word) {
       let newWord = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+      newWord = newWord.replace(/\s{2,}/g," ");
       return newWord;
     }
 
