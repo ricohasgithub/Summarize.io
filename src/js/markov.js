82119868170af5b81be9864625d1af32809ba9a7
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-08-18 15:05:21 -0400

    Added Summarize Receiver End
     - Added summarier activation and prep + summarization code body in markov.js

--- src/js/markov.js
+++ src/js/markov.js
@@ -41,27 +41,20 @@ var markovjs = (function() {
 
         // Add following words for each given word
        for (j=0; j<inputText.length-1; j++) {
-
          if (inputText[j] === currWord  && count < 4) {
-
            let found = false;
            let next = inputText[j+1];
-
            for (k=0; k<wordArr.length; k++) {
              if (wordArr[k] === currWord) {
                found = true;
                break;
              }
            }
-
            if (!found) {
              wordArr[count] = next;
            }
-
          }
-
        }
-
       }
 
       this.followers = followers;
@@ -97,11 +90,25 @@ var markovjs = (function() {
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+    console.log("background listener loaded");
     if (request.sender == "content" && request.selText == "disabled") {
       // Disabled for webpage - No action
     } else if (request.sender == "content" && request.selText !== "disabled") {
       // User has selected text that is not "disabled", summarize input text (selText) and send back response
       let summary = "This is a summary";
+
+      // Summarize!
+
+      // Create and pass global parameters for trainer
+      let inputText = request.selText;
+      let capLength = inputText.length * 0.4;
+
+      // Instantiate Markov Summarizer Model
+      let summarizer = new markovjs.MarkovModel();
+
+      // Prep and train summarizer
+      summarizer.prep(inputText, capLength);
+      summarizer.summarize(inputText, capLength);
 
       // Send a response message to the content script
       sendResponse({summary: summary});
